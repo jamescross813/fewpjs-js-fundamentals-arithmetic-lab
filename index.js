78940f46c00beca43c1f18423ca79b6808f69562
jamescross <crossjames813@gmail.com>
2021-08-22 18:31:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2 ;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8004;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 4, 6, 12, 20)
+
+    
+
